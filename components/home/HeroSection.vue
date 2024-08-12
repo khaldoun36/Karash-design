642f2b0e8fe3ef79f0d67bbbb152454bd7ace14e
khaldoun36 <khaldoun36@gmail.com>
2024-08-12 20:10:58 +0400

    video added

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -6,26 +6,14 @@
         <div
             class="img-container full-width absolute inset-0 -z-10 h-full w-full overflow-clip"
         >
-            <NuxtImg
-                src="https://images.unsplash.com/photo-1604943963840-a18fe96a6322?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="kitchen design"
-                class="h-full w-full object-cover brightness-[30%]"
-                preload
-                format="webp"
-            />
-            <NuxtImg
-                src="https://images.unsplash.com/photo-1615873968403-89e068629265?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Home decor"
-                class="h-full w-full object-cover brightness-[30%]"
-                preload
-                format="webp"
-            />
-            <NuxtImg
-                src="https://plus.unsplash.com/premium_photo-1661931749081-23d69ddb62d1?q=80&w=2183&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="furniture design"
-                class="h-full w-full object-cover brightness-[30%]"
-                preload
-                format="webp"
+            <video
+                ref="videoBG"
+                :src="videoSource"
+                autoplay
+                loop
+                muted
+                class="h-full w-full object-cover brightness-[60%]"
+                id="videoBG"
             />
         </div>
         <h1
@@ -33,54 +21,22 @@
         >
             {{ $t('home_hero_title.hero_text') }}
         </h1>
-        <!-- <HomeTextFlip />
-        <p class="mt-4 max-w-[65ch] text-base text-secondary-400 lg:text-lg">
-            {{ $t('home_hero_description') }}
-        </p>
-        <div class="hero-btn-container mt-8 flex items-center">
-            <BaseButtonLink class="primary" :to="localePath(`/contact-us`)">{{
-                $t('home_hero_btn_group.btn_cta')
-            }}</BaseButtonLink>
-            <BaseButtonLink class="secondary" :to="localePath(`/projects`)">{{
-                $t('home_hero_btn_group.btn_view_projects')
-            }}</BaseButtonLink>
-        </div> -->
     </main>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+const videoBG = ref(null);
 const localePath = useLocalePath();
+
+import videoSource from '~/assets/bgVideo.mp4';
+
 onMounted(() => {
-    const images = document.querySelectorAll('.img-container img');
-    let currentIndex = 0;
-
-    // Set the initial visibility
-    images[currentIndex].style.opacity = 1;
-
-    setInterval(() => {
-        // Hide the current image
-        images[currentIndex].style.opacity = 0;
-
-        // Move to the next image
-        currentIndex = (currentIndex + 1) % images.length;
-
-        // Show the next image
-        images[currentIndex].style.opacity = 1;
-    }, 4000); // 8 seconds
+    videoBG.value.playbackRate = 0.65;
 });
 </script>
 
 <style scoped>
-.img-container img {
-    position: absolute; /* Ensure absolute positioning */
-    inset: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    opacity: 0; /* Start with all images hidden */
-    transition: opacity 1s ease-in-out; /* Smooth transition */
-}
-
 @keyframes slide-top {
     from {
         opacity: 0;
